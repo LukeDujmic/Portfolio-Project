0a2e8478c73e06b60d5c5742109c6945504df18f
Luke <leveloneboi@gmail.com>
2020-04-10 13:23:41 -0700

    Started on several changes, like javascript and animation
    Added more form inputs to the page.  Created required fields and javascript functions to check the reloading of the page.  Also remade the animation to look more smooth and not overlap.

---
+++ js/portfolioFunctionality.js
@@ -0,0 +1,12 @@
+//var navExpanded = false;
+//document.getElementById("expandNav").addEventListener("mousedown", permaExpand)
+
+/*function permaExpand()
+{
+        document.querySelectorAll("#expandNav").style = "width: 500px; height: 500px; transition: 1s;";
+        document.querySelectorAll("#cluster").style = "background-color: orange";
+        
+        console.log("successful")
+    
+}*/ //background-color: rgba(255, 255, 255, 0.466); height: 250px; width: 250px;
+
